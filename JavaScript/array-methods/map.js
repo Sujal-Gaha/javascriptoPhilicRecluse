423226be5056fb7913953c28d3f5ffd534d3e1c9
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-01-30 22:37:26 +0545

    map

--- JavaScript/array-methods/map.js
+++ JavaScript/array-methods/map.js
@@ -6,16 +6,22 @@
 
 let number = [5, 8, 3, 1, -1];
 
-let newNumbers = number.map((num) => num + 1);
+let newNumbers = number.map((num) => {
+    return num++;
+});
 console.log("The numbers are ", newNumbers);  //  New number is  [ 6, 9, 4, 2, 0 ]
 
 
 let fruits = ["apple", "banana", "cherry", "dragonfruit", "emu"];
 
-let capitalFruits = fruits.map((fruit) => fruit.toUpperCase());
+let capitalFruits = fruits.map((fruit) => {
+    return fruit.toUpperCase();
+});
 console.log("The fruits are ", capitalFruits);  //  The fruits are  [ 'APPLE', 'BANANA', 'CHERRY', 'DRAGONFRUIT', 'EMU' ]
 
 
 let nums = [7, 6, 5];
-let sqrArr = nums.map((num) => num ** 2);
+let sqrArr = nums.map((num) => {
+    return num ** 2;
+});
 console.log("The square numbers are ", sqrArr); //  The square numbers are  [ 49, 36, 25 ]

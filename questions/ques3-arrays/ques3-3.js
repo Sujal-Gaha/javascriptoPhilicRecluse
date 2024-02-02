142059a904a3a30b79b1ae8e7a2cc11079094616
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-02-02 21:09:52 +0545

    ques3-3

---
+++ questions/ques3-arrays/ques3-3.js
@@ -0,0 +1,20 @@
+/**
+ * Write a program to sort numbers in ascending order
+ */
+
+
+const arr = [76, 23, 34, 10, 85, 49];
+const length = arr.length;
+
+for (i = 0; i < length - 1; i++) {
+    for(j = i + 1; j < length; j++) {
+        
+        if (arr[i] > arr[j]) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+
+console.log("The array in ascending order is ", arr);

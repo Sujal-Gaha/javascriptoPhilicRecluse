1bf46feebda2fbd7951bc6148e132899ca7856a0
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-02-03 22:45:01 +0545

    asynchro

---
+++ JavaScript/asynchronous/4-promise-chaining.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ *  Promise chaining in JavaScript is the technique of consecutively linking multiple 
+ *  asynchronous operations with .then() to create a sequential and organized flow of 
+ *  code execution.
+ * 
+ */
+
